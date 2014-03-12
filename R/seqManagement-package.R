bef5c4460d5dedeb59213f6734bb439d6c2cd2a6
Francois Michonneau <francois.michonneau@gmail.com>
2014-03-12 02:30:00 -0400

    adding package doc

---
+++ R/seqManagement-package.R
@@ -0,0 +1,8 @@
+##' Sequence data manipulation to ease phylogenetic analyses
+##'
+##' A suite of tools to automate and facilitate data preparation.
+##' 
+##' @docType package
+##' @import ape
+##' @name seqManagement
+NULL
