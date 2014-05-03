4fa3d093c52fd7b79ea38a4313cbbd32d32f02ca
Francois Michonneau <francois.michonneau@gmail.com>
2014-05-03 13:59:05 -0400

    improved doc for cutAlignment, fixed formatting, changed need for extensions to be 3 character long

--- R/cutAlignment.R
+++ R/cutAlignment.R
@@ -2,7 +2,17 @@
 ##' Given an alignment and a partition file, takes the alignment and
 ##' cuts it into individual partitions (opposite of mergeSeq)
 ##'
-##' At this time, only takes a RAxML formatted partition file.
+##' At this time, only takes a RAxML formatted partition file. The
+##' individual partition generated are named with the full alignment
+##' followed by the partition names specified in the partition file
+##' separated by an underscore. For each of the file created, the same
+##' extension as the full alignment is used. For instance, if your
+##' alignment file is named \code{fullAlignment.phy}, and your
+##' partitions are named \code{part1}, \code{part2} and \code{part3},
+##' this function will generate the files
+##' \code{fullAlignment_part1.phy}, \code{fullAlignment_part2.phy},
+##' and \code{fullAlignment_part3.phy}.
+##' 
 ##' @title Cut an alignment into individual partitions.
 ##' @param algfile file name of the alignment to cut
 ##' @param partfile file name of the partition file
@@ -11,22 +21,29 @@
 ##' output (to be passed to ape:::write.dna)
 ##' @return TRUE if the function worked, used for its side effect of
 ##' creating individual partition files.
+##' @seealso \code{\link{write.dna}}
 ##' @author Francois Michonneau
+##' @examples
+##' \dontrun{
+##' cutAlignment("fullAlignment.phy", partfile="partInfo.part",
+##'              formatin="sequential", format="sequential", colsep="")
+##' }
 ##' @export
 cutAlignment <- function(algfile, partfile, formatin="fasta", ...) {
 
-    pInfo <- raxmlPartitionInfo(partfile)
+    pInfo <- seqManagement::raxmlPartitionInfo(partfile)
 
     pNm <- pInfo$locusName
     pBeg <- pInfo$locusStart
     pEnd <- pInfo$locusEnd
 
-    alg <- read.dna(file=algfile, format=formatin, as.matrix=TRUE)
+    alg <- ape::read.dna(file=algfile, format=formatin, as.matrix=TRUE)
 
     for (i in 1:length(pBeg)) {
         tmpAlg <- alg[, pBeg[i]:pEnd[i]]
-        ext <- gsub("(.+)\\.([a-zA-Z]{3,}$)", paste("\\1_", pNm[i], ".\\2", sep=""), algfile)
-        write.dna(tmpAlg, file=ext, ...)
+        ext <- gsub("(.+)\\.([a-zA-Z]{1,}$)",
+                    paste("\\1_", pNm[i], ".\\2", sep=""), algfile)
+        ape::write.dna(tmpAlg, file=ext, ...)
     }
     TRUE
 }
