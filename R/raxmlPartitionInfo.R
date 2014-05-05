f469aa61391c861e553e261f4f808d04aaea4d21
Francois Michonneau <francois.michonneau@gmail.com>
2014-05-05 14:29:49 -0400

    added possibility of reading codon partitions from raxml partition files

--- R/raxmlPartitionInfo.R
+++ R/raxmlPartitionInfo.R
@@ -1,8 +1,10 @@
 ##' This function takes a RAxML partition file and returns information
 ##' about it as a list.
 ##'
-##' Currently, this function ignores the type of data of each partition,
-##' and does not support the codon notation (e.g., 1-300\3).
+##' Currently, this function ignores the type of data of each
+##' partition, does not support the codon notation (e.g.,
+##' \code{1-300\3}), and requires continuous partitions (e.g.,
+##' \code{1-300} works, but \code{1-300,401-500} doesn't work).
 ##' @title Information from a RAxML partition file
 ##' @param partfile Path of a RAxML partition file
 ##' @return A list that includes the elements:
@@ -14,17 +16,24 @@
 ##' @export
 ##' @author Francois Michonneau
 raxmlPartitionInfo <- function(partfile) {
-    pfile <- scan(file=partfile, what="character", sep="\n", quiet=TRUE)
+    pfile <- readLines(partfile)
     pfile <- sapply(pfile, function(x) gsub("\\s?", "", x))
-    pLoc <- sapply(strsplit(pfile, ","), function(x) x[2])
+    pLoc <- gsub("^DNA,", "", pfile)
     pLoc <- strsplit(pLoc, "=")
     pNm <- sapply(pLoc, function(x) x[1])
     pRange <- sapply(pLoc, function(x) x[2])
-    pRange <- strsplit(pRange, "-")
-    pBeg <- lapply(pRange, function(x) x[1])
-    pBeg <- as.integer(pBeg)
-    pEnd <- lapply(pRange, function(x) x[2])
-    pEnd <- as.integer(pEnd)
-
-    list(file=partfile, locusName=pNm, locusStart=pBeg, locusEnd=pEnd)
+    pRange <- strsplit(pRange, ",")
+    pBeg <- lapply(pRange, function(rg) {
+        tmpRg <- lapply(rg, function(x) {
+            sRg <- unlist(strsplit(x, "\\\\"))
+            byRg <- ifelse(length(sRg) > 1, sRg[2], 1)
+            curRg <- unlist(strsplit(sRg[1], "-"))
+            res <- as.numeric(c(curRg, byRg))
+            names(res) <- c("begin", "end", "by")
+            res
+        })
+        tmpRg
+    })    
+    names(pBeg) <- pNm
+    pBeg
 }
