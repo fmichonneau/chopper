ab87a8a9b1204c86580e1d9acc0f84e72e43085e
Francois Michonneau <francois.michonneau@gmail.com>
2014-06-27 12:39:02 -0400

    improved output of checkAmbiguity, added tests

--- R/checkAmbiguity.R
+++ R/checkAmbiguity.R
@@ -1,25 +1,32 @@
 
 ##' Check for ambiguities or other characters that are not allowed in
-##' haplotype sequences.
+##' haplotypes.
 ##'
 ##' This function takes a DNA alignment and looks for characters in
-##' the sequence that are not A, C, T, G, -, ? (and possibly N). It
+##' the sequence that are not A, C, T, G, -, ? (and optionally N). It
 ##' ignores case.
 ##' @title Check for ambiguities in a DNA sequence
-##' @param file path and file name for a DNA alignment.
-##' @param format format of the DNA alignment
-##' @param Namb should Ns be considered ambiguities?
-##' @param quiet if FALSE, outputs as messages the ambiguities found
+##' @param file path and file name for a DNA alignment (character)
+##' @param format format of the DNA alignment (passed to
+##' \code{\link[ape]{read.dna}})
+##' @param Namb should Ns be considered ambiguities? (logical)
+##' @param quiet if \code{FALSE}, outputs as messages the ambiguities
+##' found (logical)
+##' @param simplify if \code{TRUE}, the returned object is reduced to
+##' only include the sequences that contain ambiguities. If
+##' \code{FALSE}, the returned object has the same length as the
+##' number of sequences in the alignment.
 ##' @param ... additional arguments to be passed to ape:::read.dna
 ##' @return If there are no ambiguities, the function returns a
-##' zero-length integer vector. Otherwise, if quiet=TRUE, the function
-##' returns a named vector that indicates the position in the
-##' alignment of the ambiguities for each sequence that has one or
-##' more ambiguities; if quiet=FALSE, this vector is returned
-##' invisibly in addition of the messages.
+##' zero-length integer vector. Otherwise, it returns a named list
+##' which includes for each sequence, the position in the alignment of
+##' the ambiguities for each sequence that has one or more ambiguities
+##' (if \code{simplify=TRUE}), and is named using the ambiguity. if
+##' quiet=FALSE, this vector is returned invisibly in addition of the
 ##' @author Francois Michonneau
 ##' @export
-checkAmbiguity <- function(file, format="fasta", quiet=FALSE, Namb=TRUE, ...) {
+checkAmbiguity <- function(file, format="fasta", quiet=FALSE, Namb=TRUE,
+                           simplify=TRUE, ...) {
     alg <- read.dna(file=file, format=format, as.character=TRUE, as.matrix=TRUE,
                     ...)
     if(!Namb) {
@@ -31,18 +38,29 @@ checkAmbiguity <- function(file, format="fasta", quiet=FALSE, Namb=TRUE, ...) {
     isAmbiguity <- apply(alg, 1, function(x) which(!x %in% ambChar))
     if (length(isAmbiguity)) {
         hasAmbiguity <- sapply(isAmbiguity, function(x) length(x) > 0)
-        isAmbiguity <- isAmbiguity[hasAmbiguity]
+        lstAmbiguity <- sapply(1:nrow(alg), function(x) alg[x, isAmbiguity[[x]]])
+        if (simplify) {
+            isAmbiguity <- isAmbiguity[hasAmbiguity]
+            lstAmbiguity <- lstAmbiguity[hasAmbiguity]
+        }
+        res <- mapply(function(isAmb, lstAmb) { names(isAmb) <- lstAmb; isAmb },
+                      isAmbiguity, lstAmbiguity)
         if (!quiet) {
             message("There are ambiguities.")
-            for(i in 1:length(isAmbiguity)) {
-                message(names(isAmbiguity)[i])
-                for (j in 1:length(isAmbiguity[[i]])) {
-                    message("  ", isAmbiguity[[i]][j], ": ", alg[names(isAmbiguity)[i], isAmbiguity[[i]]][j])
+            if (simplify) {
+                resTmp <- res
+            }
+            else {
+                resTmp <- res[sapply(res, function(x) length(x) > 0)]
+            }
+            for(i in 1:length(resTmp)) {
+                message(names(resTmp)[i])
+                for (j in 1:length(resTmp[[i]])) {
+                    message("  ", resTmp[[i]][j], ": ", names(resTmp[[i]][j]))
                 }
             }
-            invisible(isAmbiguity)
         }
-        else isAmbiguity   
+        invisible(res)            
     }
     else integer(0)
 }
--- tests/testthat/test.removeEmptySeqs.R
+++ tests/testthat/test.removeEmptySeqs.R
@@ -1,5 +1,5 @@
 
-context("removeEmptySeqq")
+context("removeEmptySeqs")
 
 test_that("phylip gaps as -", {
     cat("3 6",
@@ -8,14 +8,16 @@ test_that("phylip gaps as -", {
         "sp3   ------",
         sep="\n", file="testPhy.phy")
     resMade <- removeEmptySeqs("testPhy.phy", output="testPhy_red.phy",
-                               formatin="seq", formatout="seq", gap="-")
+                               formatin="sequential", gap="-")
     resOut <- ape::read.dna(file="testPhy_red.phy", format="sequential")
     expect_equal(dim(resOut), c(2, 6))
     expect_equal(dimnames(resOut)[[1]], c("sp1", "sp2"))
-    expect_warning(
+    expect_error(
         resMade <- removeEmptySeqs("testPhy.phy", formatin="seq",
-                                   quiet=FALSE, gap="-")
+                                   overwrite=FALSE, gap="-")
         )
+    resMade <- removeEmptySeqs("testPhy.phy", formatin="seq",
+                               overwrite=TRUE, gap="-")
     expect_equal(resMade, "sp3")    
     unlink("testPhy.phy")
     unlink("testPhy_red.phy")
@@ -28,14 +30,16 @@ test_that("phylip gaps as *", {
         "sp3   ******",
         sep="\n", file="testPhy.phy")
     resMade <- removeEmptySeqs("testPhy.phy", output="testPhy_red.phy",
-                               formatin="seq", formatout="seq", gap="*")
+                               formatin="sequential", gap="*")
     resOut <- ape::read.dna(file="testPhy_red.phy", format="sequential")
     expect_equal(dim(resOut), c(2, 6))
     expect_equal(dimnames(resOut)[[1]], c("sp1", "sp2"))
-    expect_warning(
+    expect_error(
         resMade <- removeEmptySeqs("testPhy.phy", formatin="seq",
-                                   quiet=FALSE, gap="*")
+                                   overwrite=FALSE, gap="*")
         )
+    resMade <- removeEmptySeqs("testPhy.phy", formatin="seq",
+                               overwrite=TRUE, gap="*")
     expect_equal(resMade, "sp3")    
     unlink("testPhy.phy")
     unlink("testPhy_red.phy")
