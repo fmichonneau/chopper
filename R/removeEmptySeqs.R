624cc2dd424f48730032d4bc2dc53ffa1061fa85
Francois Michonneau <francois.michonneau@gmail.com>
2015-10-19 18:22:39 -0400

    removeEmptySeqs doesn't need to have gap specified

--- R/removeEmptySeqs.R
+++ R/removeEmptySeqs.R
@@ -5,7 +5,7 @@
 ##' contains gaps. This function can read the same type of alignment
 ##' files as ape:::read.dna, and output the same formats as
 ##' ape:::write.dna
-##' @title Remove empty sequences 
+##' @title Remove empty sequences
 ##' @param file path and file name for a DNA alignment
 ##' @param output path and file name of the alignment with the missing
 ##' data removed. If missing, the original file is overwritten, with
@@ -20,7 +20,6 @@
 ##' override this to no character, i.e. \code{colsep=""}).
 ##' @param overwrite If no output file is specified and if overwrite is TRUE,
 ##' the original file is overwritten.
-##' @param gap character in the sequence to be considered as missing data
 ##' @param ... additional arguments to be passed to
 ##' \code{\link[ape]{write.dna}}.
 ##' @return TRUE if function succeedeed, but mostly used for its side
@@ -28,17 +27,22 @@
 ##' @author Francois Michonneau
 ##' @export
 removeEmptySeqs <- function(file, output, formatin="fasta", formatout=formatin,
-                            colsep="", overwrite=FALSE, gap, ...) {
+                            colsep="", overwrite=FALSE, ...) {
     if (missing(output)) {
          if (overwrite)  output <- file
         else stop("Indicate output file or allow file to be overwritten.")
     }
-    gapchar <- paste("\\Q", gap, "\\E", sep="")
     alg <- ape::read.dna(file, format=formatin, as.character=TRUE,
                          as.matrix=TRUE)
-    nMissing <- apply(alg, 1, function(x) length(grep(gapchar, x)))
-    whichNotMissing <- sapply(nMissing, function(x) x != dim(alg)[2])
-    alg <- alg[whichNotMissing, ]
+    ## an empty sequence is a sequence that only has one type of character
+    ## and that is not a letter
+    isNotEmpty <- apply(alg, 1, function(x) {
+        table_x <- table(x)
+        length(table_x) > 1 ||
+            any(grepl("[a-z]", names(table_x)))
+    })
+    emptySeq <- dimnames(alg)[[1]][!isNotEmpty]
+    alg <- alg[isNotEmpty, ]
     write.dna(alg, file=output, format=formatout, colsep=colsep, ...)
-    invisible(names(whichNotMissing[!whichNotMissing]))
+    invisible(emptySeq)
 }
--- tests/testthat/test.removeEmptySeqs.R
+++ tests/testthat/test.removeEmptySeqs.R
@@ -8,17 +8,17 @@ test_that("phylip gaps as -", {
         "sp3   ------",
         sep="\n", file="testPhy.phy")
     resMade <- removeEmptySeqs("testPhy.phy", output="testPhy_red.phy",
-                               formatin="sequential", gap="-")
+                               formatin="sequential")
     resOut <- ape::read.dna(file="testPhy_red.phy", format="sequential")
     expect_equal(dim(resOut), c(2, 6))
     expect_equal(dimnames(resOut)[[1]], c("sp1", "sp2"))
     expect_error(
         resMade <- removeEmptySeqs("testPhy.phy", formatin="seq",
-                                   overwrite=FALSE, gap="-")
+                                   overwrite=FALSE)
         )
     resMade <- removeEmptySeqs("testPhy.phy", formatin="seq",
-                               overwrite=TRUE, gap="-")
-    expect_equal(resMade, "sp3")    
+                               overwrite=TRUE)
+    expect_equal(resMade, "sp3")
     unlink("testPhy.phy")
     unlink("testPhy_red.phy")
 })
@@ -30,18 +30,32 @@ test_that("phylip gaps as *", {
         "sp3   ******",
         sep="\n", file="testPhy.phy")
     resMade <- removeEmptySeqs("testPhy.phy", output="testPhy_red.phy",
-                               formatin="sequential", gap="*")
+                               formatin="sequential")
     resOut <- ape::read.dna(file="testPhy_red.phy", format="sequential")
     expect_equal(dim(resOut), c(2, 6))
     expect_equal(dimnames(resOut)[[1]], c("sp1", "sp2"))
     expect_error(
         resMade <- removeEmptySeqs("testPhy.phy", formatin="seq",
-                                   overwrite=FALSE, gap="*")
+                                   overwrite=FALSE)
         )
     resMade <- removeEmptySeqs("testPhy.phy", formatin="seq",
-                               overwrite=TRUE, gap="*")
-    expect_equal(resMade, "sp3")    
+                               overwrite=TRUE)
+    expect_equal(resMade, "sp3")
     unlink("testPhy.phy")
     unlink("testPhy_red.phy")
 })
 
+test_that("a sequence with only a letter is not considered a gap", {
+    cat("3 6",
+        "sp1 aaaaaa",
+        "sp2 ----tt",
+        "sp3 ------",
+        sep = "\n", file = "testPhy.phy")
+    resMade <- removeEmptySeqs("testPhy.phy", output = "testPhy_red.phy",
+                               formatin = "sequential")
+    resOut <- ape::read.dna(file = "testPhy_red.phy", format = "sequential")
+    expect_equal(dim(resOut), c(2, 6))
+    expect_equal(dimnames(resOut)[[1]], c("sp1", "sp2"))
+    unlink("testPhy.out")
+    unlink("testPhy_red.out")
+})
