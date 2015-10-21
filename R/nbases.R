a38627aa02e9523de77421657a5f10a830bbc3aa
Francois Michonneau <francois.michonneau@gmail.com>
2015-10-21 12:51:13 -0400

    add nbase function to compute number of base pairs
    in each sequence of an alignment

---
+++ R/nbases.R
@@ -0,0 +1,22 @@
+##' Calculates the number of base pairs in each sequence of an alignment.
+##'
+##' The argument \code{gap} should be a valid regular expression whose
+##' value is substituted (by \code{gsub}) into empty characters. The
+##' remaining is the base pairs making up the sequences.
+##'
+##' @title Number of bases
+##' @param file A file with DNA sequences
+##' @param gap  The character(s) used as gaps in the file
+##' @param format The file format of the alignment
+##' @param ... Additional parameters to be passed to \code{\link[ape]{read.dna}}
+##' @return a named vector indicating the number of non-gap characters found in each sequence.
+##' @author Francois Michonneau
+##' @export
+nbase <- function(file, gap="-", format="fasta", ...) {
+    alg <- ape::read.dna(file=file, format="fasta", as.character=TRUE, ...)
+    lgt <- apply(alg, 1, function(x) {
+        .seq <- gsub(gap, "", paste0(x,collapse=""))
+        nchar(.seq)
+    })
+    lgt
+}
